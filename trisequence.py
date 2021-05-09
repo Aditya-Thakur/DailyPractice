f870cb2f6ef28fe5efe37d0384d5aa8f89faca40
Aditya <aditya@Adityas-MacBook-Air.local>
2021-05-10 01:22:45 +0530

    #1

---
+++ trisequence.py
@@ -0,0 +1,25 @@
+def nextVal(x):
+    nextTerm = x
+    lastDigit = x%10
+    x = x//10
+    if(lastDigit == 0):
+        return 1
+    if(lastDigit == 1):
+        nextTerm = x*10 + 3
+    elif(lastDigit == 3):
+        nextTerm = x*10 + 4
+    else:
+        nextTerm = nextVal(x)*10 + 1
+    return nextTerm
+    
+
+def getSeqTerm(n):
+    nextTerm = 1
+    while(n>1):
+        n = n - 1
+        nextTerm = nextVal(nextTerm)
+    return nextTerm
+
+print('4th term is ',getSeqTerm(4))
+print('10th term is ',getSeqTerm(10))
+
